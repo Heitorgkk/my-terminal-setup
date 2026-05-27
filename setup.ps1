0d7e17b3f1750e61c0756b4a60d957c8260f0a48
Arthur Pagliarini Martins <arthurp@weg.net>
2026-05-27 18:36:56 -0300

    hot fixes v1000

--- setup.ps1
+++ setup.ps1
@@ -21,8 +21,11 @@ Write-Host "$PROFILE" -ForegroundColor DarkMagenta
 Write-Host "Instalando Fastfetch..."
 winget install Fastfetch-cli.Fastfetch --source winget --disable-interactivity
 
+# atualizar PATH dps do winget instalar o fastfetch
+
+$env:PATH = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
+
 # definir arquivo de configuração do fastfetch para .\config
-powershell
 fastfetch --config .\config.jsonc
 
 Clear-Host
